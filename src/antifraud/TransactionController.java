59204a09e17370488d8d67279f862916cb81fbe5
Jakub Łabuz <jakub.k.labuz@gmail.com>
2023-03-13 16:30:02 +0100

    Add TransactionController with ValidationResult DTO

---
+++ src/antifraud/TransactionController.java
@@ -0,0 +1,24 @@
+package antifraud;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/antifraud")
+public class TransactionController {
+
+    private final TransactionValidator transactionValidator;
+
+    public TransactionController(TransactionValidator transactionValidator) {
+        this.transactionValidator = transactionValidator;
+    }
+
+    @PostMapping("transaction")
+    public ValidationResult processTransaction(@Valid @RequestBody Transaction transaction) {
+        return new ValidationResult(transactionValidator.validate(transaction.getAmount()));
+    }
+}
---
+++ src/antifraud/ValidationResult.java
@@ -0,0 +1,35 @@
+package antifraud;
+
+import java.util.Objects;
+
+public class ValidationResult {
+    private TransactionStatus result;
+
+    public ValidationResult(TransactionStatus result) {
+        this.result = result;
+    }
+
+    public ValidationResult() {
+    }
+
+    public TransactionStatus getResult() {
+        return result;
+    }
+
+    public void setResult(TransactionStatus result) {
+        this.result = result;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ValidationResult result1 = (ValidationResult) o;
+        return result == result1.result;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(result);
+    }
+}
