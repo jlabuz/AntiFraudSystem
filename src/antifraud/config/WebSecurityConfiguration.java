e9e1ef7a4d53cbef1a966a09c8a4010b08df6e05
Jakub Łabuz <jakub.k.labuz@gmail.com>
2023-03-13 17:04:10 +0100

    Add required HttpSecurity configuration

---
+++ src/antifraud/config/WebSecurityConfiguration.java
@@ -0,0 +1,37 @@
+package antifraud.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.servlet.http.HttpServletResponse;
+
+@Configuration
+public class WebSecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.httpBasic()
+                .authenticationEntryPoint(restAuthenticationEntryPoint()) // Handles auth error
+                .and()
+                .csrf().disable().headers().frameOptions().disable() // for Postman, the H2 console
+                .and()
+                .authorizeRequests() // manage access
+                .antMatchers(HttpMethod.POST, "/api/auth/user").permitAll()
+                .antMatchers("/actuator/shutdown").permitAll() // needs to run test
+                .anyRequest().denyAll()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS); // no session
+
+        return http.build();
+    }
+
+    private AuthenticationEntryPoint restAuthenticationEntryPoint() {
+        return (request, response, authException) -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
+    }
+}
