91fc75cec03426034fdd2c92daf7ed61e601953a
Jakub Łabuz <jakub.k.labuz@gmail.com>
2023-03-13 16:26:47 +0100

    Add model class

---
+++ src/antifraud/Transaction.java
@@ -0,0 +1,23 @@
+package antifraud;
+
+import javax.validation.constraints.Min;
+
+class Transaction {
+    @Min(value = 1)
+    private long amount;
+
+    public Transaction(long amount) {
+        this.amount = amount;
+    }
+
+    public Transaction() {
+    }
+
+    public long getAmount() {
+        return amount;
+    }
+
+    public void setAmount(long amount) {
+        this.amount = amount;
+    }
+}
