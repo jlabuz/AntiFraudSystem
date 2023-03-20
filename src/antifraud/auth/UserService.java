66b646385be42c2315f4ad0e246ada5804036443
Jakub Łabuz <jakub.k.labuz@gmail.com>
2023-03-20 02:55:31 +0100

    Add logic for changing user role in UserService

--- src/antifraud/auth/UserService.java
+++ src/antifraud/auth/UserService.java
@@ -1,5 +1,6 @@
 package antifraud.auth;
 
+import antifraud.auth.exceptions.RoleAlreadyAssignedException;
 import antifraud.auth.exceptions.UsernameAlreadyUsedException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -54,8 +55,27 @@ public class UserService implements UserDetailsService {
     @Transactional
     public void deleteUser(String username) {
         User user = userRepository.findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException("Username n  ot found"));
+                .orElseThrow(() -> new UsernameNotFoundException("Username not found"));
 
         userRepository.delete(user);
+    }
+
+    @Transactional
+    public User changeUserRole(String username, String roleName) {
+        Role role = Role.valueOf(roleName);
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Username not found"));
+        validateRoleChange(role, user);
+        user.setRole(role);
+        return userRepository.save(user);
+    }
+
+    private static void validateRoleChange(Role role, User user) {
+        if (role == Role.ADMINISTRATOR) {
+            throw new IllegalArgumentException();
+        }
+        if (role.equals(user.getRole())) {
+            throw new RoleAlreadyAssignedException();
+        }
     }
 }
---
+++ src/antifraud/auth/exceptions/RoleAlreadyAssignedException.java
@@ -0,0 +1,4 @@
+package antifraud.auth.exceptions;
+
+public class RoleAlreadyAssignedException extends RuntimeException {
+}
